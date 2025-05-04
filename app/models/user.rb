4256bd25713e4ebf740664e8946ac11b728f120a
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-04 18:49:05 -0500

    feat: user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 3, maximum: 30 }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8, maximum: 72 }
 end
